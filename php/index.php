90d2e8c4f439c0cdfaad69fb92bdabf3351d9db7
freed0m666 <a427an@gmail.com>
2023-05-03 08:08:47 +0700

    db_connect

--- php/index.php
+++ php/index.php
@@ -1,2 +1,21 @@
 <?php
-print('hello world');
+$title="Мои растения";
+$mysql = new mysqli("mysql", "root", "qwer", "flower");
+$result = $mysql->query("SELECT `id`,`data`,`image`,`content` FROM flower_table");
+print("<html lang='ru'>
+<head>
+    <title>$title</title>
+</head>
+<body>");
+if($result){
+    while($row = $result->fetch_assoc()){
+        print("<img alt='images' src='images/".$row["image"]."' width='640' height='480'><br>");
+        print($row["data"]."||".$row["content"]);
+        print("<br>");
+    }
+}
+$mysql->close();
+print("
+    </body>
+</html>
+");
